0a239946236728c663cd7e0aa6e996a7c03ac3d7
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2021-01-31 22:49:23 +1100

    footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Banner from './components/banner/Banner';
 import Info from './components/info/Info';
 import Testimonals from './components/testimonial/Testimonals';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <Banner></Banner>
     <Info></Info>
     <Testimonals></Testimonals>
+    <Footer></Footer>
     
      
     </div>
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Footer.scss"
+
+function Footer() {
+    return (
+        <div className="footer">
+        <h2>V + R</h2>
+        <h6>Copyright @ 2021</h6>
+            
+        </div>
+    )
+}
+
+export default Footer
