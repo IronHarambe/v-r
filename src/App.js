5e14f8db35294ea2da507f498b3dabab1de75030
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2021-02-01 10:02:02 +1100

    contact and work pages init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './components/header/Header';
 import Banner from './components/banner/Banner';
 import Info from './components/info/Info';
 import Testimonals from './components/testimonial/Testimonals';
 import Footer from './components/footer/Footer';
+import Contact from './components/pages/contact/Contact';
+import Work from './components/pages/work/Work';
 
 function App() {
   return (
     <Router>
     <div className="App">
+    <Switch>
+
+    <Route path="/contact">
+      <Header></Header>
+      <Contact></Contact>
+      <Footer></Footer>
+    </Route>
+
+    <Route path="/work">
+      <Header></Header>
+      <Work></Work>
+      <Footer></Footer>
+    </Route>
+
+
+    <Route path="/">
     <Header></Header>
     <Banner></Banner>
     <Info></Info>
     <Testimonals></Testimonals>
     <Footer></Footer>
-    
-     
+    </Route>
+     </Switch>
     </div>
     </Router>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 import "./Header.scss"
 
@@ -9,16 +10,16 @@ function Header() {
          {/* Main settings for navbar */}
       <Navbar collapseOnSelect expand="sm" bg="light" variant="light" className="nav" fixed="top"    >
         {/* Logo */}
-        <Navbar.Brand href="#home" className="logo">V+R</Navbar.Brand>
+        <Navbar.Brand  className="logo">V+R</Navbar.Brand>
         {/* Responsive bar */}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" className="toggler" />
         {/* Collapsing navbar options */}
         <Navbar.Collapse id="responsive-navbar-nav" >
           <Nav className="mr-auto" ></Nav>
           <Nav>
-            <Nav.Link href="#home">HOME</Nav.Link>
-            <Nav.Link href="#projects">WORK</Nav.Link>
-            <Nav.Link href="#skill">CONTACT</Nav.Link>
+            <Nav.Link as={NavLink} to="/">HOME</Nav.Link>
+            <Nav.Link as={NavLink} to="/work">WORK</Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">CONTACT</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
